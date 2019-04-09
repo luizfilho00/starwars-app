2701f2499cc3704d3bcf91848bdd2b90676b54c1
Luiz Filho <luiizfiilhoo@gmail.com>
2019-04-09 19:00:14 -0400

    Layout inicial e exibição dos posteres

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:star_wars/ui/home_page.dart';
 
-void main() => runApp(MaterialApp(
-      home: HomePage(),
-    ));
+void main() => runApp(
+    MaterialApp(home: HomePage(), theme: ThemeData(hintColor: Colors.yellow)));
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -17,15 +17,82 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     _getMovies().then((map) {
-      print(map);
+      //print(map);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.asset('lib/assets/logo.png'),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "Pesquisar filme:",
+                    labelStyle: TextStyle(color: Colors.yellow),
+                    border: OutlineInputBorder()),
+                style: TextStyle(color: Colors.yellow, fontSize: 18.0),
+                textAlign: TextAlign.center,
+              )),
+          Expanded(
+            child: FutureBuilder(
+                future: _getMovies(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                          width: 300.0,
+                          height: 300.0,
+                          alignment: Alignment.center,
+                          child: CircularProgressIndicator(
+                            valueColor: AlwaysStoppedAnimation<Color>(
+                                Colors.yellowAccent),
+                            strokeWidth: 5.0,
+                          ));
+                    default:
+                      if (snapshot.hasError)
+                        return Container();
+                      else
+                        return _createMovieTable(context, snapshot);
+                  }
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+_createMovieTable(BuildContext context, AsyncSnapshot snapshot) {
+  var count = snapshot.data["count"];
+  return GridView.builder(
+      padding: EdgeInsets.all(5.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 5.0,
+        mainAxisSpacing: 5.0,
+      ),
+      itemCount: count,
+      itemBuilder: (context, index) {
+        var imgPath =
+            "lib/assets/" + snapshot.data["results"][index]["title"] + ".jpg";
+        return GestureDetector(
+          child: Image(
+            image: AssetImage(imgPath),
+          ),
+        );
+      });
+}
+
+_createMovieInfo(BuildContext context, AsyncSnapshot snapshot) {}
